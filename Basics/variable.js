ee5844558b751d6e718aaf7cc0f1e26af6f19f59
waliiiddd <queryofwalid@gmail.com>
2026-07-02 02:13:13 +0000

    setup CodeSpace and learned about variables

---
+++ Basics/variable.js
@@ -0,0 +1,19 @@
+const accountId = 1114
+let accountEmail = "walid@gmail.com"
+var accountPassword = "545644"
+
+//use let when declaring in variable cause it solves the problems of block scope and functional scope.
+
+//accountId = 444  //not allowed cause we have already declared accountId as const so it can't be changed.
+
+accontCity = "Chittagong"
+
+accountEmail = "binwalid@gmail.com"
+
+accountPassword = "646444"
+
+let accountState;
+
+console.log(accountId,accontCity,accountEmail,accountPassword)
+
+console.table([accountId,accontCity,accountEmail,accountPassword,accountState])
